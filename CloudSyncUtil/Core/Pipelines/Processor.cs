191361e2ab7d1db498242d6e84ea3ea265c313cd
Andrey Bobrov <andreybobrovdev@gmail.com>
2016-03-06 19:40:11 +0300

    Pipeline processor now has a priority initializer

--- CloudSyncUtil/Core/Pipelines/Processor.cs
+++ CloudSyncUtil/Core/Pipelines/Processor.cs
@@ -6,17 +6,24 @@ using System.Threading.Tasks;
 
 namespace CloudSyncUtil.Core.Pipelines
 {
-    public abstract class Processor<TArgs> where TArgs:PipelineArgs
+    public abstract class Processor<TArgs> where TArgs : PipelineArgs
     {
         public int Priority { get; set; }
 
-        protected abstract void DoProcess();
+        protected Processor() : this(100) { }
+
+        protected Processor(int priority)
+        {
+            Priority = priority;
+        }
+
+        protected abstract void DoProcess(TArgs args);
 
         public virtual void Process(TArgs args)
         {
             if (!args.Cancel)
             {
-                DoProcess();
+                DoProcess(args);
             }
         }
     }
