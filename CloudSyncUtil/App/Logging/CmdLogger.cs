277d27de0757b5e4b4b95391f37b12ef925b985a
broAir <andrey.bobrov.dev@gmail.com>
2016-03-28 12:22:17 +0800

    Logger added

---
+++ CloudSyncUtil/App/Logging/CmdLogger.cs
@@ -0,0 +1,41 @@
+﻿using CloudSyncUtil.Core.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CloudSyncUtil.App.Logging
+{
+    public class CmdLogger : ILogger
+    {
+        protected void Write(string msg)
+        {
+            Console.WriteLine(msg);
+        }
+
+        public void Error(string msg)
+        {
+            var message = string.Format(" [ERROR] An error occurred: {0}", msg);
+            this.Write(message);
+        }
+
+        public void Info(string msg)
+        {
+            var message = string.Format(" [INFO] {0}", msg);
+            this.Write(message);
+        }
+
+        public void Success(string msg)
+        {
+            var message = string.Format(" [SUCCESS] {0}", msg);
+            this.Write(message);
+        }
+
+        public void Warn(string msg)
+        {
+            var message = string.Format(" [WARN] {0}", msg);
+            this.Write(message);
+        }
+    }
+}
---
+++ CloudSyncUtil/Core/Logging/ILOgger.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CloudSyncUtil.Core.Logging
+{
+    interface ILogger
+    {
+        void Info(string msg);
+        void Warn(string msg);
+        void Error(string msg);
+        void Success(string msg);
+    }
+}
